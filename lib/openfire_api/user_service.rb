044b7c35db4f23092cb380e10aa253e0360f30b7
Paul Asmuth <paul@23linesofcode.com>
2011-10-16 19:50:44 +0200

    fixed indentation

--- lib/openfire_api/user_service.rb
+++ lib/openfire_api/user_service.rb
@@ -1,76 +1,77 @@
 class OpenfireApi::UserService
-	
-	@@api_path = "plugins/userService/userservice"
-	@@api_exceptions = %w(UserServiceDisabled RequestNotAuthorised IllegalArgumentException UserNotFoundException UserAlreadyExistsException)
-	
-	class HTTPException < StandardError; end
-	class InvalidResponseException < StandardError; end
-	class UserServiceDisabledException < StandardError; end
-	class RequestNotAuthorisedException < StandardError; end
-	class IllegalArgumentException < StandardError; end
-	class UserNotFoundException < StandardError; end
-	class UserAlreadyExistsException < StandardError; end
-	
-	def initialize(options=Hash.new)
-		@options = { :path => @@api_path }.merge(options)
-	end
-	
-	def add_user!(opts)
-		submit_request(opts.merge(:type => :add))
-	end
-	
-	def delete_user!(opts)
-		submit_request(opts.merge(:type => :delete))
-	end
-	
-	def update_user!(opts)
-		submit_request(opts.merge(:type => :update))
-	end
-	
-	def lock_user!(opts)
-		submit_request(opts.merge(:type => :disable))
-	end
-	
-	def unlock_user!(opts)
-		submit_request(opts.merge(:type => :enable))
-	end
-	
-private
-	
-	def build_query(params)				
-		"#{build_query_uri.to_s}?#{build_query_params(params)}"
-	end
-	
-	def build_query_uri
-		uri = URI.parse(@options[:url])
-		uri.path = File.join(uri.path, @@api_path)
-		uri
-	end
-	
-	def build_query_params(params)
-		params.merge!(:secret => @options[:secret])
-		params.to_a.map{ |p| "#{p[0]}=#{p[1]}" }.join('&')	
-	end
-	
-	def submit_request(params)
-   	data = submit_http_request(build_query_uri, build_query_params(params))
-   	parse_response(data)
-	end
-	
-	def submit_http_request(uri, params)
-    http = Net::HTTP.new(uri.host, uri.port)
- 	  resp, data = http.get("#{uri.path}?#{params}")
- 	  return data
-	rescue Exception => e
-		raise HTTPException, e.to_s
-	end
-	
-	def parse_response(data)
-		error = data.match(/<error>(.*)<\/error>/)
-		eval "raise #{error[1].gsub('Exception', '')}Exception" if error && @@api_exceptions.include?(error[1]) # HACK !!! ;)
-		raise InvalidResponseException unless data.match(/<result>ok<\/result>/)
-   	return true
+  
+  @@api_path = "plugins/userService/userservice"
+  @@api_exceptions = %w(UserServiceDisabled RequestNotAuthorised IllegalArgumentException UserNotFoundException UserAlreadyExistsException)
+  
+  class HTTPException < StandardError; end
+  class InvalidResponseException < StandardError; end
+  class UserServiceDisabledException < StandardError; end
+  class RequestNotAuthorisedException < StandardError; end
+  class IllegalArgumentException < StandardError; end
+  class UserNotFoundException < StandardError; end
+  class UserAlreadyExistsException < StandardError; end
+  
+  def initialize(options=Hash.new)
+    @options = { :path => @@api_path }.merge(options)
   end
-	
-	
-end	
+  
+  def add_user!(opts)
+    submit_request(opts.merge(:type => :add))
+  end
+  
+  def delete_user!(opts)
+    submit_request(opts.merge(:type => :delete))
+  end
+  
+  def update_user!(opts)
+    submit_request(opts.merge(:type => :update))
+  end
+  
+  def lock_user!(opts)
+    submit_request(opts.merge(:type => :disable))
+  end
+  
+  def unlock_user!(opts)
+    submit_request(opts.merge(:type => :enable))
+  end
+  
+private
+  
+  def build_query(params)       
+    "#{build_query_uri.to_s}?#{build_query_params(params)}"
+  end
+  
+  def build_query_uri
+    uri = URI.parse(@options[:url])
+    uri.path = File.join(uri.path, @@api_path)
+    uri
+  end
+  
+  def build_query_params(params)
+    params.merge!(:secret => @options[:secret])
+    params.to_a.map{ |p| "#{p[0]}=#{p[1]}" }.join('&')  
+  end
+  
+  def submit_request(params)
+    data = submit_http_request(build_query_uri, build_query_params(params))
+    parse_response(data)
+  end
+  
+  def submit_http_request(uri, params)
+    http = Net::HTTP.new(uri.host, uri.port)
+    resp, data = http.get("#{uri.path}?#{params}")
+    return data
+  rescue Exception => e
+    raise HTTPException, e.to_s
+  end
+  
+  def parse_response(data)
+    error = data.match(/<error>(.*)<\/error>/)
+    if error && @@api_exceptions.include?(error[1])
+      raise eval("#{error[1].gsub('Exception', '')}Exception")
+    end
+    raise InvalidResponseException unless data.match(/<result>ok<\/result>/)
+    return true
+  end
+  
+end
--- spec/user_service_spec.rb
+++ spec/user_service_spec.rb
@@ -2,78 +2,78 @@ require ::File.expand_path('../spec_helper', __FILE__)
 
 describe "User Service" do
 
-	before :all do
-		@user_service = OpenfireApi::UserService.new(:url => "http://fakehost.int:2323/", :secret => "bigsecret")
-	end
-	
-	it "should build query urls" do
-		url = @user_service.send(:build_query_uri)
-		url.to_s.should == "http://fakehost.int:2323/plugins/userService/userservice"
-	end
-	
-	it "should build query params" do
-		params = @user_service.send(:build_query_params, :type => :add, :username => "user", :password => "pw")
-		params.should == "type=add&username=user&password=pw&secret=bigsecret"
-	end
-	
-	it "should build queries" do
-		url = @user_service.send(:build_query, :type => :add, :username => "user", :password => "pw")
-		url.should == "http://fakehost.int:2323/plugins/userService/userservice?type=add&username=user&password=pw&secret=bigsecret"
-	end
-	
-	it "should submit requests" do
-		FakeWeb.register_uri(:get, "http://fakehost.int:2323/plugins/userService/userservice?type=add&username=user&password=pw&secret=bigsecret", :body => "<result>ok</result>")
-		@user_service.send(:submit_request, :type => :add, :username => "user", :password => "pw").should == true
-	end
-	
-	it "should add users" do
-		FakeWeb.register_uri(:get, "http://fakehost.int:2323/plugins/userService/userservice?type=add&username=user&password=pw&secret=bigsecret", :body => "<result>ok</result>")
-		@user_service.add_user!(:username => "user", :password => "pw").should == true
-	end
-	
-	it "should delete users" do
-		FakeWeb.register_uri(:get, "http://fakehost.int:2323/plugins/userService/userservice?type=delete&username=user&secret=bigsecret", :body => "<result>ok</result>")
-		@user_service.delete_user!(:username => "user").should == true
-	end
-	
-	it "should update users" do
-		FakeWeb.register_uri(:get, "http://fakehost.int:2323/plugins/userService/userservice?type=update&username=user&password=pw&name=horst&secret=bigsecret", :body => "<result>ok</result>")
-		@user_service.update_user!(:username => "user", :password => "pw", :name => "horst").should == true
-	end
-	
-	it "should lock users" do
-		FakeWeb.register_uri(:get, "http://fakehost.int:2323/plugins/userService/userservice?type=disable&username=user&secret=bigsecret", :body => "<result>ok</result>")
-		@user_service.lock_user!(:username => "user").should == true
-	end
-	
-	it "should unlock users" do
-		FakeWeb.register_uri(:get, "http://fakehost.int:2323/plugins/userService/userservice?type=enable&username=user&secret=bigsecret", :body => "<result>ok</result>")
-		@user_service.unlock_user!(:username => "user").should == true
-	end	
-	
-	it "should handle the error: user service disabled" do
-		FakeWeb.register_uri(:get, "http://fakehost.int:2323/plugins/userService/userservice?username=user1&type=disable&secret=bigsecret", :body => "<error>UserServiceDisabled</error>")
-		lambda{ @user_service.lock_user!(:username => "user1") }.should raise_error(OpenfireApi::UserService::UserServiceDisabledException)
-	end
-	
-	it "should handle the error: request not authorized" do
-		FakeWeb.register_uri(:get, "http://fakehost.int:2323/plugins/userService/userservice?username=user1&type=disable&secret=bigsecret", :body => "<error>RequestNotAuthorised</error>")
-		lambda{ @user_service.lock_user!(:username => "user1") }.should raise_error(OpenfireApi::UserService::RequestNotAuthorisedException)
-	end
-	
-	it "should handle the error: illegal argument" do
-		FakeWeb.register_uri(:get, "http://fakehost.int:2323/plugins/userService/userservice?username=user1&type=disable&secret=bigsecret", :body => "<error>IllegalArgumentException</error>")
-		lambda{ @user_service.lock_user!(:username => "user1") }.should raise_error(OpenfireApi::UserService::IllegalArgumentException)
-	end
-	
-	it "should handle the error: user not found" do
-		FakeWeb.register_uri(:get, "http://fakehost.int:2323/plugins/userService/userservice?username=user1&type=disable&secret=bigsecret", :body => "<error>UserNotFoundException</error>")
-		lambda{ @user_service.lock_user!(:username => "user1") }.should raise_error(OpenfireApi::UserService::UserNotFoundException)
-	end
-	
-	it "should handle the error: user already exists" do
-		FakeWeb.register_uri(:get, "http://fakehost.int:2323/plugins/userService/userservice?username=user1&type=disable&secret=bigsecret", :body => "<error>UserAlreadyExistsException</error>")
-		lambda{ @user_service.lock_user!(:username => "user1") }.should raise_error(OpenfireApi::UserService::UserAlreadyExistsException)
-	end
+  before :all do
+    @user_service = OpenfireApi::UserService.new(:url => "http://fakehost.int:2323/", :secret => "bigsecret")
+  end
+  
+  it "should build query urls" do
+    url = @user_service.send(:build_query_uri)
+    url.to_s.should == "http://fakehost.int:2323/plugins/userService/userservice"
+  end
+  
+  it "should build query params" do
+    params = @user_service.send(:build_query_params, :type => :add, :username => "user", :password => "pw")
+    params.should == "type=add&username=user&password=pw&secret=bigsecret"
+  end
+  
+  it "should build queries" do
+    url = @user_service.send(:build_query, :type => :add, :username => "user", :password => "pw")
+    url.should == "http://fakehost.int:2323/plugins/userService/userservice?type=add&username=user&password=pw&secret=bigsecret"
+  end
+  
+  it "should submit requests" do
+    FakeWeb.register_uri(:get, "http://fakehost.int:2323/plugins/userService/userservice?type=add&username=user&password=pw&secret=bigsecret", :body => "<result>ok</result>")
+    @user_service.send(:submit_request, :type => :add, :username => "user", :password => "pw").should == true
+  end
+  
+  it "should add users" do
+    FakeWeb.register_uri(:get, "http://fakehost.int:2323/plugins/userService/userservice?type=add&username=user&password=pw&secret=bigsecret", :body => "<result>ok</result>")
+    @user_service.add_user!(:username => "user", :password => "pw").should == true
+  end
+  
+  it "should delete users" do
+    FakeWeb.register_uri(:get, "http://fakehost.int:2323/plugins/userService/userservice?type=delete&username=user&secret=bigsecret", :body => "<result>ok</result>")
+    @user_service.delete_user!(:username => "user").should == true
+  end
+  
+  it "should update users" do
+    FakeWeb.register_uri(:get, "http://fakehost.int:2323/plugins/userService/userservice?type=update&username=user&password=pw&name=horst&secret=bigsecret", :body => "<result>ok</result>")
+    @user_service.update_user!(:username => "user", :password => "pw", :name => "horst").should == true
+  end
+  
+  it "should lock users" do
+    FakeWeb.register_uri(:get, "http://fakehost.int:2323/plugins/userService/userservice?type=disable&username=user&secret=bigsecret", :body => "<result>ok</result>")
+    @user_service.lock_user!(:username => "user").should == true
+  end
+  
+  it "should unlock users" do
+    FakeWeb.register_uri(:get, "http://fakehost.int:2323/plugins/userService/userservice?type=enable&username=user&secret=bigsecret", :body => "<result>ok</result>")
+    @user_service.unlock_user!(:username => "user").should == true
+  end 
+  
+  it "should handle the error: user service disabled" do
+    FakeWeb.register_uri(:get, "http://fakehost.int:2323/plugins/userService/userservice?username=user1&type=disable&secret=bigsecret", :body => "<error>UserServiceDisabled</error>")
+    lambda{ @user_service.lock_user!(:username => "user1") }.should raise_error(OpenfireApi::UserService::UserServiceDisabledException)
+  end
+  
+  it "should handle the error: request not authorized" do
+    FakeWeb.register_uri(:get, "http://fakehost.int:2323/plugins/userService/userservice?username=user1&type=disable&secret=bigsecret", :body => "<error>RequestNotAuthorised</error>")
+    lambda{ @user_service.lock_user!(:username => "user1") }.should raise_error(OpenfireApi::UserService::RequestNotAuthorisedException)
+  end
+  
+  it "should handle the error: illegal argument" do
+    FakeWeb.register_uri(:get, "http://fakehost.int:2323/plugins/userService/userservice?username=user1&type=disable&secret=bigsecret", :body => "<error>IllegalArgumentException</error>")
+    lambda{ @user_service.lock_user!(:username => "user1") }.should raise_error(OpenfireApi::UserService::IllegalArgumentException)
+  end
+  
+  it "should handle the error: user not found" do
+    FakeWeb.register_uri(:get, "http://fakehost.int:2323/plugins/userService/userservice?username=user1&type=disable&secret=bigsecret", :body => "<error>UserNotFoundException</error>")
+    lambda{ @user_service.lock_user!(:username => "user1") }.should raise_error(OpenfireApi::UserService::UserNotFoundException)
+  end
+  
+  it "should handle the error: user already exists" do
+    FakeWeb.register_uri(:get, "http://fakehost.int:2323/plugins/userService/userservice?username=user1&type=disable&secret=bigsecret", :body => "<error>UserAlreadyExistsException</error>")
+    lambda{ @user_service.lock_user!(:username => "user1") }.should raise_error(OpenfireApi::UserService::UserAlreadyExistsException)
+  end
 
 end
