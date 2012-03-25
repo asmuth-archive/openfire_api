68f2bb43f26fd6f21aa28b0793dc27dd5b5cc020
Intrepidd <adrien.siami@gmail.com>
2012-03-25 19:42:08 +0200

    Fixing a compatibility problem w/ ruby 1.9.3, since the HTTP::Net api has changed, this was tested w/ ruby 1.9.2 and 1.9.3

--- lib/openfire_api/user_service.rb
+++ lib/openfire_api/user_service.rb
@@ -1,8 +1,8 @@
 class OpenfireApi::UserService
-  
+
   @@api_path = "plugins/userService/userservice"
   @@api_exceptions = %w(UserServiceDisabled RequestNotAuthorised IllegalArgumentException UserNotFoundException UserAlreadyExistsException)
-  
+
   class HTTPException < StandardError; end
   class InvalidResponseException < StandardError; end
   class UserServiceDisabledException < StandardError; end
@@ -10,61 +10,62 @@ class OpenfireApi::UserService
   class IllegalArgumentException < StandardError; end
   class UserNotFoundException < StandardError; end
   class UserAlreadyExistsException < StandardError; end
-  
+
   def initialize(options=Hash.new)
     @options = { :path => @@api_path }.merge(options)
   end
-  
+
   def add_user!(opts)
     submit_request(opts.merge(:type => :add))
   end
-  
+
   def delete_user!(opts)
     submit_request(opts.merge(:type => :delete))
   end
-  
+
   def update_user!(opts)
     submit_request(opts.merge(:type => :update))
   end
-  
+
   def lock_user!(opts)
     submit_request(opts.merge(:type => :disable))
   end
-  
+
   def unlock_user!(opts)
     submit_request(opts.merge(:type => :enable))
   end
-  
+
 private
-  
-  def build_query(params)       
+
+  def build_query(params)
     "#{build_query_uri.to_s}?#{build_query_params(params)}"
   end
-  
+
   def build_query_uri
     uri = URI.parse(@options[:url])
     uri.path = File.join(uri.path, @@api_path)
     uri
   end
-  
+
   def build_query_params(params)
     params.merge!(:secret => @options[:secret])
-    params.to_a.map{ |p| "#{p[0]}=#{p[1]}" }.join('&')  
+    params.to_a.map{ |p| "#{p[0]}=#{p[1]}" }.join('&')
   end
-  
+
   def submit_request(params)
     data = submit_http_request(build_query_uri, build_query_params(params))
     parse_response(data)
   end
-  
+
   def submit_http_request(uri, params)
-    http = Net::HTTP.new(uri.host, uri.port)
-    resp, data = http.get("#{uri.path}?#{params}")
-    return data
+    res = Net::HTTP.start(uri.host, uri.port) do |http|
+      http.get("#{uri.path}?#{params}")
+    end
+    return res.body
   rescue Exception => e
     raise HTTPException, e.to_s
   end
-  
+
   def parse_response(data)
     error = data.match(/<error>(.*)<\/error>/)
     if error && @@api_exceptions.include?(error[1])
@@ -73,5 +74,5 @@ private
     raise InvalidResponseException unless data.match(/<result>ok<\/result>/)
     return true
   end
-  
+
 end
